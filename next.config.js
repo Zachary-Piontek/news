6b427e28982264dbb2ca9bdaeab5af18c297f573
Zachary Piontek <zakpiontek@gmail.com>
2023-03-26 19:10:38 -0700

    changed image loading

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['storage.googleapis.com', 'images.gmanews.tv', 'media.cnn.com', 'www.militarytimes.com'],
+    domains: [
+      "storage.googleapis.com",
+      "images.gmanews.tv",
+      "media.cnn.com",
+      "www.militarytimes.com",
+      "sports.inquirer.net",
+      "business.inquirer.net",
+      "entertainment.inquirer.net",
+      "lifestyle.inquirer.net",
+      "opinion.inquirer.net",
+      "technology.inquirer.net",
+      "globalnation.inquirer.net",
+      "newsinfo.inquirer.net",
+    ],
   },
 };
 
--- src/app/components/RssFeeds.js
+++ src/app/components/RssFeeds.js
@@ -1,10 +1,11 @@
 import xml2js from "xml2js";
 import styles from "../page.module.css";
 import sanitizeHtml from "sanitize-html";
+import Image from "next/image.js";
 
 export default async function RssFeeds() {
   const inquirer = await fetch("https://www.inquirer.net/fullfeed/", {
-    next: { revalidate: 120 },
+    cache: "no-cache",
   });
   const inquirerText = await inquirer.text();
   const inquirerJson = await xml2js.parseStringPromise(inquirerText);
@@ -12,7 +13,7 @@ export default async function RssFeeds() {
   const rssCombined = await fetch(
     "https://rss.feedspot.com/folder/5RfLtmMZ6w==/rss/rsscombiner",
     {
-      next: { revalidate: 120 },
+      cache: "no-cache",
     }
   );
   const rssCombinedText = await rssCombined.text();
@@ -29,12 +30,17 @@ export default async function RssFeeds() {
             typeof item.description === "string"
               ? sanitizeHtml(item.description)
               : item.description;
-          console.log(item); // add this line to log the contents of the item object
           return (
             <li key={index}>
               <p className="title">{item.title}</p>
               <a href={item.link}>
-                <img src={mediaContent.url} alt={item.title} />
+                {/* <img src={mediaContent.url} alt={item.title} /> */}
+                <Image
+                  src={mediaContent.url}
+                  alt={item.title}
+                  width={300}
+                  height={300}
+                />
               </a>
               <a href={item.link}>Read More</a>
               <p
@@ -48,12 +54,25 @@ export default async function RssFeeds() {
       </ul>
       <ul>
         {rssCombinedJson.rss.channel[0].item.map((item, index) => {
+          const sanitizedDescription =
+            typeof item.description === "string"
+              ? sanitizeHtml(item.description)
+              : item.description;
           return (
             <li key={index}>
               <p className="title">{item.title}</p>
-              <img src={item.enclosure[0].$.url} alt={item.title} />
+              {/* <img src={item.enclosure[0].$.url} alt={item.title} /> */}
+              <Image
+                src={item.enclosure[0].$.url}
+                alt={item.title}
+                width={300}
+                height={300}
+              />
               <a href={item.link}>Read More</a>
-              <p className="description">{item.description}</p>
+              <p
+                className="description"
+                dangerouslySetInnerHTML={{ __html: sanitizedDescription }}
+              ></p>
               <p>{new Date(item.pubDate).toLocaleString()}</p>
             </li>
           );
