ae82665256f4bb76cce07fb2689ea89802fc5f07
Zachary Piontek <zakpiontek@gmail.com>
2023-04-13 13:42:46 -0700

    added sports feed page

--- src/app/sports/page.js
+++ src/app/sports/page.js
@@ -1,9 +1,47 @@
-export default function Sports() {
+import xml2js from "xml2js";
+import styles from "../page.module.css";
+import Image from "next/image";
+
+export default async function SportsFeed() {
+  const rssCombined = await fetch(
+    "https://rss.app/feeds/kJZXUQf66Ju4PjMr.xml",
+    {
+      cache: "no-cache",
+    }
+  );
+  const rssCombinedText = await rssCombined.text();
+  const rssCombinedJson = await xml2js.parseStringPromise(rssCombinedText);
+
+  console.log(rssCombinedJson.rss.channel[0].item);
+
   return (
-    <main>
-      <div>
-        <h1>Sports page</h1>
-      </div>
-    </main>
+    <div className={styles.rssfeeds}>
+      <ul className={styles.newspaperPage}>
+        {rssCombinedJson.rss.channel[0].item.map((item, index) => {
+          const mediaContent =
+            item["media:content"] && item["media:content"][0];
+          // Convert item.description to a string and remove HTML tags
+          const description = item.description
+            .toString()
+            .replace(/<[^>]*>?/gm, "");
+          return (
+            <li key={index}>
+              <p className="title">{item.title}</p>
+              <p className="description">{description}</p>
+              {mediaContent && (
+                <Image
+                  src={mediaContent.$.url}
+                  alt={item.title}
+                  width={200}
+                  height={200}
+                />
+              )}
+              <a href={item.link}>Read More</a>
+              <p>{new Date(item.pubDate).toLocaleString()}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
